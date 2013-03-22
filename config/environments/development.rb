476e780fb1e91d4ac5823bcc69d99e2451003cdf
Julian Jünemann <jjuenemann@gmail.com>
2013-03-22 10:40:24 +0700

    solved Paperclip problem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,7 @@ Omrails::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
+  Paperclip.options[:command_path] = "/usr/local/bin"
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
